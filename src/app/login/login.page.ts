9efd51366faaabc23853b115b2c6e46280c351bf
Golenia <lucas.golenia@atos.net>
2019-11-13 13:09:57 +0100

    password at least 6 chars

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -27,7 +27,7 @@ export class LoginPage implements OnInit {
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
       ])),
       password: new FormControl('', Validators.compose([
-        Validators.minLength(5),
+        Validators.minLength(6),
         Validators.required
       ])),
     });
@@ -41,7 +41,7 @@ export class LoginPage implements OnInit {
     ],
     'password': [
       { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+      { type: 'minlength', message: 'Password must be at least 6 characters long.' }
     ]
   };
  
